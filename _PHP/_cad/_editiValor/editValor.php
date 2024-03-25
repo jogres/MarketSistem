16733f287cb9fba10fab971f55ab8418caaefd74
jogres <maninho_wesley@hotmail.com>
2024-03-25 18:57:08 -0300

    cracao de subpasta para a edicao do valor metodo para subtracao do valor em porcentagem backup do banco para trabalhar em outros locais

---
+++ _PHP/_cad/_editiValor/editValor.php
@@ -0,0 +1,28 @@
+<?php 
+  
+  $conn = new mysqli("localhost", "root", "", "market");
+  
+  $Porcent = floatval($_POST['Porcent'])/100;
+  $id = $_POST['id'];
+  $edit = mysqli_query($conn, "select cadprod.idProd, cadprod.idFor, cadprod.NomeProd, cadfor.NomeFor, cadprod.DesProd, cadprod.ContProd, cadprod.CodBar, cadprod.Preco FROM cadfor INNER JOIN cadprod ON cadfor.idfor = CadProd.idfor WHERE CadProd.idFor= '$id'");
+  
+  if(isset($_POST['add'])){
+    while($campo = mysqli_fetch_array($edit)){
+
+      $porV = $campo['Preco']*$Porcent;
+      $novoV = $porV + $campo['Preco'];
+      mysqli_query($conn, "UPDATE cadprod SET Preco = '$novoV' WHERE idFor ='$id' AND idProd = '".$campo['idProd']."'");
+    }
+    
+  }
+  elseif(isset($_POST['sub'])){
+    while($campo = mysqli_fetch_array($edit)){
+
+      $porV = $campo['Preco']*$Porcent;
+      $novoV = $campo['Preco'] - $porV;
+      mysqli_query($conn, "UPDATE cadprod SET Preco = '$novoV' WHERE idFor ='$id' AND idProd = '".$campo['idProd']."'");
+    }
+  } 
+
+?>
+
