95e4afc87fab4119d991b07b997ad55f3864946b
wesley rocha <maninho_wesley@hotmail.com>
2024-11-10 14:39:37 -0300

    tentativa de imprimir agora saber se ta certo

--- _PHP/_venda/envVenda.php
+++ _PHP/_venda/envVenda.php
@@ -11,6 +11,7 @@
   $Total = $_POST['total'];
   $Troco = $_POST['troco'];
   $nomeCli = $_POST['nomeCli'];
+  $produtos = explode("\n", $ProdV);
   
   if (isset($_POST['submitted'])) {
     if(isset($_POST['Fim'])){
@@ -31,6 +32,41 @@
        $_SESSION['id_temp'] = array();
     } 
     
+
+    $printer = fopen("COM1", "w"); // Supondo que a impressora esteja na porta COM1
+
+    if ($printer) {
+        // Configurar impressora: Definir largura de impressão, alinhar e formatar
+        fwrite($printer, "\x1B\x40"); // Inicializar impressora (ESC @)
+        fwrite($printer, "\x1B\x61\x01"); // Alinhar centro (ESC a 1)
+
+        // Imprimir cabeçalho com o nome da empresa
+        fwrite($printer, $Nome . "\n");
+        fwrite($printer, "Data: " . $DataV . "\n");
+        fwrite($printer, "Cliente: " . $nomeCli . "\n");
+        fwrite($printer, "------------------------------\n");
+
+        // Imprimir produtos formatados
+        fwrite($printer, "\x1B\x61\x00"); // Alinhar à esquerda (ESC a 0)
+        foreach ($produtos as $produto) {
+            // Exemplo de formatação: Qtd Nome Prod ..... Preço
+            // Exemplo: 2x Produto A ........... R$ 10,00
+            fwrite($printer, $produto . "\n");
+        }
+
+        // Imprimir total e troco
+        fwrite($printer, "------------------------------\n");
+        fwrite($printer, "Total: R$ " . $Total . "\n");
+        fwrite($printer, "Troco: R$ " . $Troco . "\n");
+        fwrite($printer, "------------------------------\n");
+
+        // Finalizar impressão
+        fwrite($printer, "\x1D\x56\x01"); // Cortar papel (GS V 1)
+        fclose($printer);
+    } else {
+        echo "Erro ao conectar com a impressora.";
+    }
+
     if(isset($_POST['submitte'])){
       foreach ($_SESSION['id'] as $id) {
         $edit = mysqli_query($conn, "SELECT cadprod.idProd, cadprod.ContProd, cadprod.Preco FROM cadprod WHERE cadprod.idProd= '$id'");
